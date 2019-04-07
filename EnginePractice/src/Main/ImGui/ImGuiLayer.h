1e0fbc321e873aa5a2c95b0cb3e846274b7140d6
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-04-07 13:18:54 -0400

    ImGui Events

--- EnginePractice/src/Main/ImGui/ImGuiLayer.h
+++ EnginePractice/src/Main/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Main/Layer.h"
 
+#include "Main/Events/ApplicationEvent.h"
+#include "Main/Events/KeyEvent.h"
+#include "Main/Events/MouseEvent.h"
+
 namespace EnginePractice {
 
 	class EP_API ImGuiLayer : public Layer {
@@ -13,6 +17,17 @@ namespace EnginePractice {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 	};
